61b9d43c984fea5599770b8c8a40dfab2223c23a
Rahul More <rahulmore2405@gmail.com>
2025-07-14 17:21:47 +0530

    Initial commit with all 4 Kafka producer projects

---
+++ Employee_Kafka_Producer_Integration_WithKey_AsynchronousCommunication-main/README.md
@@ -0,0 +1,8 @@
+This code represents an integration of kafka with spring boot.
+
+This is only a producer and not consumer.
+This project is sending data with key and communicates asynchronously. It means spring boot app will not wait for kafka to respond, it will send a message and continue to function normally.
+
+Technologies: mySQL server 9.3, Java 17, spring boot 3.5.3, Postman and Kafka 3.6.0
+
+Kafka has 3 servers running in cluster.
---
+++ Employee_Kafka_Producer_Integration_WithoutKey_AsynchronousCommunication-main/src/main/java/com/employee/ws/service/EmployeeServiceImpl.java
@@ -0,0 +1,32 @@
+package com.employee.ws.service;
+
+import java.util.concurrent.ExecutionException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.employee.ws.entity.Employee;
+import com.employee.ws.kafka.KafkaProducerService;
+import com.employee.ws.repository.EmployeeRepository;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+	
+	@Autowired
+	EmployeeRepository employeeRepository;
+	
+	@Autowired
+	KafkaProducerService kafkaProducerService;
+	
+	@Override
+	public Employee addNewEmployee(Employee employee) {
+		
+	Employee newEmployee = employeeRepository.save(employee);
+		
+	//Kafka code here
+	kafkaProducerService.sendEmployeeEvent(newEmployee);
+	
+	return newEmployee;
+	}
+
+}
